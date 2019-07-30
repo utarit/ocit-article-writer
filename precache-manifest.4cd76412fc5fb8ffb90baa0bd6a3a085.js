f88212eca5a401d6aefcda9a8effecdc6f1d8aeb
mert <utarit.543@gmail.com>
2019-07-30 23:10:19 +0300

    Updates

---
+++ precache-manifest.4cd76412fc5fb8ffb90baa0bd6a3a085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a66655dae752f596426a6fd6c0910b",
+    "url": "/ocit-article-writer/index.html"
+  },
+  {
+    "revision": "7e48b4ad47199f512da1",
+    "url": "/ocit-article-writer/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "ee8f009f0d32cb786a68",
+    "url": "/ocit-article-writer/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7e48b4ad47199f512da1",
+    "url": "/ocit-article-writer/static/js/2.4504f77f.chunk.js"
+  },
+  {
+    "revision": "ee8f009f0d32cb786a68",
+    "url": "/ocit-article-writer/static/js/main.315eb81b.chunk.js"
+  },
+  {
+    "revision": "2307620ae1f0e8de967c",
+    "url": "/ocit-article-writer/static/js/runtime~main.8dfd6a9b.js"
+  }
+]);
